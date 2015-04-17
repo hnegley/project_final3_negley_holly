ebd2f28ec376d3a5c10de5c55209567c86675da5
Holly Negley <holly.negley@icloud.com>
2015-04-17 00:55:39 -0400

    added hours to contact page, added styling to contact page, started adding isotope plugin in menu page

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+      $('#map_canvas').googleMaps({
+        latitude: 42.351505,
+        longitude: -71.094455
+        });
+
+      $container.isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows',
+        ...
+        })
+}
+
+
