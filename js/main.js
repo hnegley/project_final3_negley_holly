1d668eea745c27efdc1b0e43f1641668ce8cc439
Holly Negley <holly.negley@icloud.com>
2015-04-18 23:40:09 -0400

    changed the size of the map, tried to fix the map from Richard's email

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ $(document).ready(function() {
         longitude: -95.5477
         });
 
+      function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+          center: new google.maps.LatLng(33.6625, -95.5547),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+         var map = new google.maps.Map(mapCanvas, mapOptions)
+            }
+        google.maps.event.addDomListener(window, 'load', initialize);
+
 });
 
 
