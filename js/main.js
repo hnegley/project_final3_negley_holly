edd782d77b1ff57e85c026eebc1a692f02c7c926
Holly Negley <holly.negley@icloud.com>
2015-04-18 23:00:06 -0400

    updated location for google map, added backstretch and sticky plugins, having trouble with main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
+      $.backstretch("images/yellow.jpg");
+
+      $("#sticker").sticky({ topSpacing: 0, center:true, className:"hey" });
+
       $('#map_canvas').googleMaps({
         latitude: 33.6625,
-        longitude: 95.5477
+        longitude: -95.5477
         });
 
-}
+});
 
 
